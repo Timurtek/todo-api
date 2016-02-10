106d7dc96ff8b9cc1f63e5bd8fd4ad8435f979a1
Timurtek <timurbizel88@gmail.com>
2016-02-09 22:26:34 -0800

    able to delete todo items

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var _ = require('underscore');
+var _=require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,6 +20,7 @@ app.get('/',function(req,res){
 app.get('/todos',function(req,res){
   res.json(todos);
 });
+
 //GET /todos/:id
 app.get('/todos/:id',function(req,res){
   //req parameters are always string
@@ -33,13 +34,28 @@ app.get('/todos/:id',function(req,res){
     res.status(404).send('Not Found');
   }
 });
+
+//DELETE /todos/:id
+app.delete('/todos/:id',function(req,res){
+  var todoId = parseInt(req.params.id,10);
+  var matchedTodo = _.findWhere(todos,{id:todoId});
+
+  if(matchedTodo){
+    //removes the matched array item from the array list
+    //then adds the array list back to todos var
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }else{
+    res.status(404).send('Not Found');
+  }
+});
 // POST new todo item
 app.post('/todos', function (req, res) {
   var body = _.pick(req.body,'description','completed');
 
   if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
     return res.status(400).send('Something went wrong');
-  }
+}
 
   //set body.description to be trimmed.
     body.description = body.description.trim();
@@ -84,6 +100,7 @@ app.post('/profiles/',function(req,res){
   console.log(body);
   res.json(body);
 });
+
 
 //listens for server
 app.listen(PORT,function(){
